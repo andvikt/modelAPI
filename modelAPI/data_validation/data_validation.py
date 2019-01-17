a642e96f691476e51e2c1d05b5da89d3010b5f27
viktorov-ag <andvikt@gmail.com>
2019-01-17 11:00:39 +0300

    added saving/loading functionality

--- modelAPI/data_validation/data_validation.py
+++ modelAPI/data_validation/data_validation.py
@@ -1,4 +1,5 @@
 from .helpers import _PrepValidator, _LoggerMixin
+from ..utils.saver import _SelfSaving
 from pandas import DataFrame, concat
 from typing import Sequence
 from modelAPI.data_validation.helpers import VAL_LOGGER, LEVELS
@@ -6,7 +7,7 @@ from logging import Logger
 import os
 
 
-class DataValidator(_LoggerMixin):
+class DataValidator(_LoggerMixin, _SelfSaving):
     """
     Валидатор, запоминает статистику о данных, чтобы в будущем на новых данных проверить отклонение от базовых статистик
     Для расчета и проверки статистик используется val_schema, может быть определена пользователем, но лучше всего
@@ -25,15 +26,16 @@ class DataValidator(_LoggerMixin):
 
     """
     def __init__(self
-                 , init_data: DataFrame
-                 , val_schema: Sequence[_PrepValidator]
-                 , name: str
+                 , init_data: DataFrame = None
+                 , val_schema: Sequence[_PrepValidator] = None
+                 , name: str = 'validate'
                  ):
         """
         :param init_data: initial data for calculating base statics
         :param val_schema: validation schema
         """
         super().__init__(name=name, namespace='validator')
+        _SelfSaving.__init__(self, save_list={'validators', 'name'})
         self.logger.info(f'prepare validation data')
         self.validators = [
             x(data=init_data) for x in val_schema
