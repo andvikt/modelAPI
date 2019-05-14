330cd65727f9dd3cf58261e1678f7cf8623c5f0d
Викторов Андрей Германович <Viktorov.A.G@omega.sbrf.ru>
2019-05-14 09:04:31 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='modelAPI',
-    version='0.2.2',
+    version='0.2.3',
     packages=['modelAPI', 'modelAPI.utils', 'modelAPI.data_validation'],
-    install_requirements=['pandas', 'voluptuous', 'dill'],
+    install_requires=['pandas', 'voluptuous', 'dill'],
     url='',
     license='',
     author='viktorov-ag',
